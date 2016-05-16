44ea76b0ef73b2fa582773de11eed1580fe9318a
Inysiaaa <Inysiaaa@gmail.com>
2016-05-16 21:30:15 +0300

    Create IndexController.php

---
+++ site2/Controller/IndexController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class IndexController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        $args = array(
+            'number' => 1234,
+            'name' => 'Mike'
+        );
+
+        return $this->render('index', $args);
+    }
+    public function contactAction(Request $request)
+    {
+        $flashMessage = $request->get('flash');
+        $form = new ContactForm($request);
+        if ($request->isPost()) {
+            if ($form->isValid()) {
+                $flashMessage = 'Success';
+                Router::redirect('/index.php?route=index/contact&flash=' . $flashMessage);
+            }
+            $flashMessage = 'Error';
+        }
+        $args = array(
+            'form' => $form,
+            'flashMessage' => $flashMessage
+        );
+        return $this->render('contact', $args);
+    }
+}
